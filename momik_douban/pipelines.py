27a92284e665c185839697ee1abd7d6126765635
Achrou <2643053021@qq.com>
2020-12-02 02:13:45 +0800

    增加环境变量配置

--- momik_douban/pipelines.py
+++ momik_douban/pipelines.py
@@ -5,6 +5,7 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 import json
+import os
 from momik_douban.GistClient import GistClient
 
 
@@ -16,23 +17,25 @@ class JsonPipeline(object):
 
     @classmethod
     def from_crawler(cls, crawler):
+        gist_key = crawler.settings.get('GIST_KEY')
+        gist_id = crawler.settings.get('GIST_ID')
         return cls(
-            gist_key=crawler.settings.get('GIST_KEY'),
-            gist_id=crawler.settings.get('GIST_ID')
+            gist_key=os.getenv('GIST_KEY') if len(gist_key) == 0 else gist_key,
+            gist_id=os.getenv('GIST_ID') if len(gist_id) == 0 else gist_id
         )
 
-    def process_item(self, item, spider):
-        key = item['type']
-        if not self.files.__contains__(key):
-            self.files[key] = {'line': 1, 'page': 1, 'items': []}
-        file = self.files[key]
-        file.items.append(dict(item))
-        if file.line % 15 == 0:
-            file_name = 'item_' + key + '_' + str(file.page) + '.json'
-            self.gclient.update(self.gist_id,
-                                {"files": {file_name: {"content": json.dumps(file.items, ensure_ascii=False)}}})
-            file.items.page += 1
-            file.items = []
-        file.items.line += 1
+        def process_item(self, item, spider):
+            key = item['type']
+            if not self.files.__contains__(key):
+                self.files[key] = {'line': 1, 'page': 1, 'items': []}
+            file = self.files[key]
+            file.items.append(dict(item))
+            if file.line % 15 == 0:
+                file_name = 'item_' + key + '_' + str(file.page) + '.json'
+                self.gclient.update(self.gist_id,
+                                    {"files": {file_name: {"content": json.dumps(file.items, ensure_ascii=False)}}})
+                file.items.page += 1
+                file.items = []
+            file.items.line += 1
 
-        return item
+            return item
